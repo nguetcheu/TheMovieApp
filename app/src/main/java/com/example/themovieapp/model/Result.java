8fb7f61af8d49ca2bc33b33a8e5b09e9b728f9ed
nguetcheu <dnguetcheu@gmail.com>
2025-02-25 21:39:56 +0100

    first commit

---
+++ app/src/main/java/com/example/themovieapp/model/Result.java
@@ -0,0 +1,59 @@
+package com.example.themovieapp.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+public class Result {
+
+    @SerializedName("page") // link key in the json response to the pojo variable of class
+    @Expose // ensure that variable is serialize and deserialize
+    private Integer page;
+
+    @SerializedName("total_pages")
+    @Expose
+    private Integer totalPages;
+
+    @SerializedName("total_results")
+    @Expose
+    private Integer totalResults;
+
+    @SerializedName("results")
+    @Expose
+    private List<Movie> results = null;
+
+    public Result() {
+    }
+
+    public Integer getPage() {
+        return page;
+    }
+
+    public void setPage(Integer page) {
+        this.page = page;
+    }
+
+    public List<Movie> getResults() {
+        return results;
+    }
+
+    public void setResults(List<Movie> results) {
+        this.results = results;
+    }
+
+    public Integer getTotalPages() {
+        return totalPages;
+    }
+
+    public void setTotalPages(Integer totalPages) {
+        this.totalPages = totalPages;
+    }
+
+    public Integer getTotalResults() {
+        return totalResults;
+    }
+
+    public void setTotalResults(Integer totalResults) {
+        this.totalResults = totalResults;
+    }
+}
---
+++ app/src/main/java/com/example/themovieapp/serviceapi/MovieApiService.java
@@ -0,0 +1,17 @@
+package com.example.themovieapp.serviceapi;
+
+import com.example.themovieapp.model.Result;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface MovieApiService {
+
+   // The service interface defines the structure
+   // and behavior of the API requests.
+   // Acts as a bridge between your app and the API
+
+    @GET("movie/popular")
+    Call<Result> getPopularMovies(@Query("api_key") String apiKey);
+}
